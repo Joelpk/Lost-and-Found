658f4740a176006a817211589e2cd2d159ffb3dd
Joel Peka <joelpeka753@gmail.com>
2025-11-25 10:07:44 +0100

    Prepared for Vercel deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path"
 
 const app = express();
 const PORT = 3000;
@@ -23,6 +24,12 @@ app.get("/reportLostItem.ejs", (req, res) =>{
   res.render("reportLostItem.ejs");
 })
 
-app.listen(PORT, () =>{
-  console.log(`Server running on port ${PORT}`)
-});
+export default app;
+
+const isDirectRun = (process.argv[1] && process.argv[1].endsWith('server.js'));
+if (isDirectRun) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
